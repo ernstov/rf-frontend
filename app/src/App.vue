7a3a5084b83b16243dc74332f5cfaf09fd05a18c
Ernst <voroniukernst@gmail.com>
2022-03-15 17:31:48 +0000

    feat: frontend setup

---
+++ app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <div class="container__content text-pink-400">
+      Hello
+    </div>
+  </div>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container{
+  &__content{
+    font-size: 2rem;
+    // color: $color-primary;
+  }
+}
+</style>
